f0d69baffa6ee6e809fcdb56d4b6e402687d1463
Bekzhan <b.kadirgaliyev@chocofood.kz>
2020-12-06 20:20:30 +0600

    init commit

---
+++ src/core/README.md
@@ -0,0 +1,1 @@
+No more than 5 apps per django project
---
+++ src/core/helpers/default_apps.py
@@ -0,0 +1,13 @@
+from typing import Tuple
+
+DJANGO_APPS: Tuple[str, ...] = (
+    'django.contrib.admin', 'django.contrib.staticfiles',
+    'django.contrib.contenttypes', 'django.contrib.auth',
+    'django.contrib.messages', 'django.contrib.sessions',
+)
+SIDE_APPS: Tuple[str, ...] = (
+    'corsheaders', 'rest_framework', 'django_extensions',
+    'django_filters', 'django_fsm', 'silk',
+)
+PROJECT_APPS: Tuple[str, ...] = ('common',)
+DEFAULT_APPS: Tuple[str, ...] = DJANGO_APPS + SIDE_APPS + PROJECT_APPS
